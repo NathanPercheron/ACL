85b9e4b4b4f73698a0c25cdc7768667a0364f6ec
Nathan Percheron <percheron.nathan@outlook.fr>
2019-06-04 17:40:14 +0200

    Form create partenaire

--- src/Controller/PartenaireController.php
+++ src/Controller/PartenaireController.php
@@ -2,18 +2,57 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Partenaire;
+use App\Form\PartenaireType;
+use App\Repository\PartenaireRepository;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PartenaireController extends AbstractController
 {
     /**
      * @Route("/partenaire", name="partenaire")
      */
-    public function index()
+    public function GetPartenaires(PartenaireRepository $repo)
     {
+        $partenaire = $repo->findAll();
         return $this->render('partenaire/partenaire.html.twig', [
             'controller_name' => 'PartenaireController',
+            'partenaire' => $partenaire
         ]);
+    }
+
+    /**
+     * @Route("/partenaire/new", name="partenaire_new")
+     */
+    public function CreatePartenaire(Request $request){
+        // Création du formulaire
+        $partenaire = new Partenaire();
+        $form = $this->createForm(PartenaireType::class, $partenaire);
+
+        // Envoie du formulaire
+        $form->handleRequest($request);
+        if ($form->isSubmitted()){
+            $file=$partenaire->getImage();
+            $file = $request->files->get('partenaire')['image'];
+
+            $uploads_directory = $this->getParameter('uploads_directory');
+            $filename = md5(uniqid()).'.'. $file->guessExtension();
+            $file->move($uploads_directory, $filename);
+
+            $partenaire->setImage($filename);
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($partenaire);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('partenaire');
+        }
+        return $this->render(
+            'partenaire/new.html.twig',
+            array('form' => $form->createView())
+        );
     }
 }
---
+++ src/Form/PartenaireType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Partenaire;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class PartenaireType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom', TextType::class)
+            ->add('image', FileType::class,array ('label' => 'Veuillez ajouter une image'))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Partenaire::class,
+        ]);
+    }
+}
